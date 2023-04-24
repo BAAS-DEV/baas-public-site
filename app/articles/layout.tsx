73e378f69cae8e99c00d9d2837eb9ed680ffcb98
Tom Miller <wthomasmiii@gmail.com>
2023-04-23 22:18:23 -0500

    more seo

--- app/articles/layout.tsx
+++ app/articles/layout.tsx
@@ -8,6 +8,9 @@ export async function generateMetadata({}) {
     authors: { name: "BAAS Software Inc." },
     robots: { index: true },
     openGraph: {
+      title: "Home | BAAS",
+      description:
+        "Helpful articles for those interested in custom software development services",
       images: [
         "https://imagedelivery.net/6zvbH8ejfUWPqBF2dDyuGg/5683665d-2cdd-499c-b282-b7d8549dd700/public",
       ],
--- app/page.tsx
+++ app/page.tsx
@@ -10,12 +10,14 @@ import ArticlesHero from "../lib/Sections/ArticlesHero";
 
 export const metadata: Metadata = {
   title: "Home | BAAS",
-  description: " Web, Mobile, & Custom Software Agency",
+  description: "Custom Software Development Agency",
   authors: { name: "BAAS Software Inc." },
   robots: { index: true },
   openGraph: {
+    title: "Home | BAAS",
+    description: "Custom Software Development Agency",
     images: [
-      "https://imagedelivery.net/6zvbH8ejfUWPqBF2dDyuGg/5683665d-2cdd-499c-b282-b7d8549dd700/public",
+      "https://imagedelivery.net/6zvbH8ejfUWPqBF2dDyuGg/e1991de4-205c-4676-3d1b-629634ccdf00/public",
     ],
   },
 };
