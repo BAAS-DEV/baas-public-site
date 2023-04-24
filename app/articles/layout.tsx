9b2d5e34edd73294992901dba8b1c58337bbb731
Tom Miller <wthomasmiii@gmail.com>
2023-04-23 23:56:39 -0500

    articles fix?

--- app/articles/layout.tsx
+++ app/articles/layout.tsx
@@ -4,15 +4,16 @@ import SEO from "../../lib/Utils/SEO";
 
 const seo = new SEO();
 
-export const metadata: Metadata = seo.GenerateSEO({
-  title: "Articles",
-  description:
-    "Free articles that intend to help you build your dream solutions",
-  images:
-    "https://imagedelivery.net/6zvbH8ejfUWPqBF2dDyuGg/d29dec25-872f-4fb6-a52d-83c6b355d500/public",
-  tags: [],
-});
-
+export async function generateMetadata() {
+  return seo.GenerateSEO({
+    title: "Articles",
+    description:
+      "Free articles that intend to help you build your dream solutions",
+    images:
+      "https://imagedelivery.net/6zvbH8ejfUWPqBF2dDyuGg/d29dec25-872f-4fb6-a52d-83c6b355d500/public",
+    tags: [],
+  });
+}
 export default function RootLayout({
   children,
 }: {
