54b41111a9d2097db00c8dbbd45b4faedf16a2e9
Tom Miller <wthomasmiii@gmail.com>
2023-04-25 21:12:08 -0500

    fixed spacing

--- lib/Sections/SlantedDiv.tsx
+++ lib/Sections/SlantedDiv.tsx
@@ -1,7 +1,7 @@
 export default function SlantedDiv() {
   return (
     <div
-      className="bg-white w-full h-96 mb-48"
+      className="bg-white w-full h-32 mb-48"
       style={{
         WebkitClipPath: "polygon(0 0, 100% 0, 100% 76%, 0% 100%)",
         clipPath: "polygon(0 0, 100% 0, 100% 76%, 0% 100%)",
